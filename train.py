06c0a3c58750b69ce745e79b0b76e237a10333d9
waiting-gy <904450727@qq.com>
2019-10-21 14:48:01 +0800

    Update train.py

--- train.py
+++ train.py
@@ -187,7 +187,8 @@ def train():
 
         if iteration % 10 == 0:
             print('timer: %.4f sec.' % (t1 - t0))
-            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data[0]), end=' ')
+            #print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data[0]), end=' ')
+            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.item()), end=' ')
 
         if args.visdom:
             #update_vis_plot(iteration, loss_l.data[0], loss_c.data[0],
